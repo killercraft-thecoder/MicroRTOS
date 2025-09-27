62212ebc1eab439cd6a524812bf98cf6bda2e2ee
killercraft-thecoder <Anonymous>
2025-09-27 11:59:09 -0700

    U

--- include/mustinclude.h
+++ include/mustinclude.h
@@ -1,6 +1,12 @@
 #ifndef MUSTINCLUDE_H
 #define MUSTINCLUDE_H
 
+#ifdef UF2_BOOTLOADER
+  #define USER_VECT_TAB_ADDRESS  0x2000U  // 8 KB offset
+#else
+  #define USER_VECT_TAB_ADDRESS  0x00U
+#endif
+
 #include "CMSIS/system_stm32f4xx.h"
 
 #endif
